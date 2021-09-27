c4795254a4c6da48df6a29c00035397b9ffe2768
mtfoley <mtfoley.mae@gmail.com>
2021-09-27 00:15:05 -0400

    feat: result details

--- src/components/SearchResultDetail.js
+++ src/components/SearchResultDetail.js
@@ -1,4 +1,4 @@
-import { PeopleIcon, StarIcon, RepoIcon, LocationIcon } from "@primer/octicons-react";
+import { PeopleIcon, PersonIcon, RepoIcon, LocationIcon, LinkExternalIcon, MarkGithubIcon } from "@primer/octicons-react";
 import classNames from "../styles";
 function SearchResultDetail({userDetails}){
     const statsImageUrl = (login)=>{
@@ -6,24 +6,27 @@ function SearchResultDetail({userDetails}){
     }
     return (
         <div className={classNames.searchResultDetail}>
-            <p>
-                <h3 className={classNames.searchResultUsername}>{userDetails.login}</h3>
-                <img
-                    src={userDetails.avatar_url}
-                    alt={userDetails.login} 
-                    className={classNames.searchResultDetailImage} />            
-            </p>
-            <p>
+            <img
+                src={userDetails.avatar_url}
+                alt={userDetails.login} 
+                className={classNames.searchResultDetailImage} />   
+            <div className={classNames.searchResultDetailData}>
+                <span className={classNames.searchResultUsername}>
+                    <PersonIcon size={16} /> {userDetails.login}
+                </span>
                 {userDetails.name && <p className={classNames.searchResultDetailLabel}>{userDetails.name}</p>}
-                {userDetails.location && <p className={classNames.searchResultDetailLabel}><LocationIcon size={16} /> {userDetails.location}</p>}
                 {userDetails.bio && <p className={classNames.searchResultDetailBio}>{userDetails.bio}</p>}
-
-            </p>
-            <ul>
-                <li><PeopleIcon size={16} /> {userDetails.followers} followers</li>
-                <li><PeopleIcon size={16} /> {userDetails.following} following</li>
-                <li><RepoIcon size={16} /> {userDetails.public_repos || "0"}</li>
-            </ul>
+                
+                <ul className="text-gray-700">
+                    {userDetails.location && <li><LocationIcon size={16} /> {userDetails.location}</li>}    
+                    <li><PeopleIcon size={16} /> {userDetails.followers} followers</li>
+                    <li><PeopleIcon size={16} /> {userDetails.following} following</li>
+                    <li><RepoIcon size={16} /> {userDetails.public_repos || "0"} public repos</li>
+                    <li><MarkGithubIcon size={16} /> <a href={userDetails.html_url} rel="noreferrer" target="_blank">GitHub Profile</a></li>
+                    {userDetails.blog && <li><LinkExternalIcon size={16} /> <a href={userDetails.blog} rel="noreferrer" target="_blank">Website</a></li>}    
+                    {userDetails.twitter_username && <li><LinkExternalIcon size={16} /> <a href={"https://twitter.com/"+userDetails.twitter_username}>@{userDetails.twitter_username}</a></li>}    
+                </ul>
+            </div>
         </div>
     );
 }
--- src/components/SearchResultList.js
+++ src/components/SearchResultList.js
@@ -3,16 +3,15 @@ import SearchResultDetail from "./SearchResultDetail";
 import classNames from "../styles";
 function SearchResultList({items,userDetails,getUserDetails,error}){
     return (
-        <div className={classNames.searchResultList}>
-            {error && <p className={classNames.errorMessage}>{error}</p>}
-            {error === null && userDetails === null
-            && <div className={classNames.resultsGrid}>
-                { items.map( result=> {
-                    return <SearchResult result={result} key={result.id} getUserDetails={getUserDetails} />
-                })}
-            </div>} 
-            {error === null && userDetails && <SearchResultDetail userDetails={userDetails} getUserDetails={getUserDetails} />}
-        </div>
-    );
+            <div className={classNames.searchResultList}>
+                {error && <p className={classNames.errorMessage}>{error}</p>}
+                {error === null && userDetails === null
+                && <div className={classNames.resultsGrid}>
+                    { items.map( result=> {
+                        return <SearchResult result={result} key={result.id} getUserDetails={getUserDetails} />
+                    })}
+                </div>} 
+            </div>
+        );
 }
 export default SearchResultList;
